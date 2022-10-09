3c20713fef083f79d7e9e78e9ae8f8fbb52a4d7c
Jae Kim <abc@example.com>
2022-10-09 16:34:16 -0700

    changed folder structure of files

---
+++ project/seed.py
@@ -0,0 +1,45 @@
+"""Seed file to initialize the db with sample data."""
+
+from models import User, Post, Tag, PostTag, db
+from app import app
+
+# Create all tables
+db.drop_all()
+db.create_all()
+
+# If table isn't empty, empty it
+Post.query.delete()
+User.query.delete()
+
+# Add users
+Dave = User(first_name='Dave', last_name="Lee")
+Jae = User(first_name='Jae', last_name="Kim")
+Joe = User(first_name='Joe', last_name="Park")
+
+# Add new objects to session, so they'll persist
+## refactor later to add all ****
+db.session.add_all([Dave,Jae,Joe])
+db.session.flush()
+
+post1 = Post(title="TestPostDave", content="This is a comment for TestPostDave", user_id = Dave.id)
+post2 = Post(title="TestPostJae", content="This is a comment for TestPostJae", user_id = Jae.id)
+post3 = Post(title="TestPostJoe", content="This is a comment for TestPostJoe", user_id = Joe.id)
+
+db.session.add_all([post1,post2,post3])
+db.session.flush()
+
+tag1 = Tag(name="fun")
+tag2 = Tag(name="quirky")
+tag3 = Tag(name="ordinary")
+
+db.session.add_all([tag1,tag2,tag3])
+db.session.flush()
+
+posttag1 = PostTag(post_id = post1.id, tag_id = tag1.id)
+posttag2 = PostTag(post_id = post2.id, tag_id = tag2.id)
+posttag3 = PostTag(post_id = post3.id, tag_id = tag3.id)
+
+db.session.add_all([posttag1,posttag2,posttag3])
+
+# Commit to the current session
+db.session.commit()
